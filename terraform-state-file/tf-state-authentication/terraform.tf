16152eea884794c4f6b3094026c62a7a736e9214
unknown <sriramarvind95@gmail.com>
2024-05-10 11:52:46 +0530

    adding and removing few sessions

---
+++ terraform-state-file/tf-state-authentication/terraform.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sriram-tf-backend"
+    key    = "key/aws_infra"
+    region = "us-east-1"
+  }
+}
